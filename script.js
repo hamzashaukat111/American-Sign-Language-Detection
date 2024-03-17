a101f0f2f66aeeb18c0a9b1415fe72e3b2018104
Hamza Shaukat <75229817+hamzashaukat111@users.noreply.github.com>
2024-03-17 19:48:11 +0500

    integrated ASL

--- script.js
+++ script.js
@@ -32,14 +32,16 @@ $(document).ready(function () {
 
   function processImage(formData) {
     $.ajax({
-      url: "https://cvscanfood-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/6ed5e91e-7c3c-44db-bcba-651a7bbb76f6/classify/iterations/Iteration1/image",
+      url: "https://westeurope.api.cognitive.microsoft.com/customvision/v3.0/Prediction/57a290ce-ba19-459a-af93-f6fc73f4e609/classify/iterations/Iteration1/image",
       type: "POST",
       data: formData,
       processData: false,
       contentType: false,
       headers: {
-        "Prediction-Key": "6d9918f13de44cdbb32081baaf81387c",
+        "Prediction-Key": "a96005bef1c34c4aa03d7481d4b4092b",
       },
+
+      // succes code
       success: function (response) {
         var predictions = response.predictions;
         var resultContainer = document.getElementById("resultContainer");
@@ -48,60 +50,105 @@ $(document).ready(function () {
         resultHeading.innerHTML = "";
 
         if (predictions.length > 0) {
-          var aliveProbability = 0;
-          var notAliveProbability = 0;
+          var maxProbability = 0;
+          var predictedTag = "";
           for (var i = 0; i < predictions.length; i++) {
             var prediction = predictions[i];
-            if (prediction.tagName === "Alive") {
-              aliveProbability = prediction.probability;
-            } else if (prediction.tagName === "notalive") {
-              notAliveProbability = prediction.probability;
+            if (prediction.probability > maxProbability) {
+              maxProbability = prediction.probability;
+              predictedTag = prediction.tagName;
             }
           }
 
           var result = '<div class="result-container">';
           result += '<div class="prediction">';
-          result += '<p class="tag-name">Alive</p>';
+          result += '<p class="tag-name">' + predictedTag + "</p>";
           result += '<div class="percentage-bar">';
           result +=
             '<div class="percentage" style="width: ' +
-            aliveProbability * 100 +
+            maxProbability * 100 +
             '%;"></div>';
           result += "</div>";
           result +=
             '<p class="probability">' +
-            (aliveProbability * 100).toFixed(2) +
-            "%</p>";
-          result += "</div>";
-
-          result += '<div class="prediction">';
-          result += '<p class="tag-name">Not Alive</p>';
-          result += '<div class="percentage-bar">';
-          result +=
-            '<div class="percentage" style="width: ' +
-            notAliveProbability * 100 +
-            '%;"></div>';
-          result += "</div>";
-          result +=
-            '<p class="probability">' +
-            (notAliveProbability * 100).toFixed(2) +
+            (maxProbability * 100).toFixed(2) +
             "%</p>";
           result += "</div>";
           result += "</div>";
 
           resultContainer.innerHTML = result;
 
-          if (aliveProbability > notAliveProbability) {
-            resultHeading.innerHTML =
-              '<h2 class="result-heading">This Video contains live person</h2>';
-          } else {
-            resultHeading.innerHTML =
-              '<h2 class="result-heading">This Video fails liveness check</h2>';
-          }
+          resultHeading.innerHTML =
+            '<h2 class="result-heading">This sign means the alphabet ' +
+            predictedTag +
+            "</h2>";
         } else {
           resultContainer.innerHTML = "<p>No predictions found.</p>";
         }
       },
+
+      //   success: function (response) {
+      //     var predictions = response.predictions;
+      //     var resultContainer = document.getElementById("resultContainer");
+      //     var resultHeading = document.getElementById("resultHeading");
+      //     resultContainer.innerHTML = "";
+      //     resultHeading.innerHTML = "";
+
+      //     if (predictions.length > 0) {
+      //       var aliveProbability = 0;
+      //       var notAliveProbability = 0;
+      //       for (var i = 0; i < predictions.length; i++) {
+      //         var prediction = predictions[i];
+      //         if (prediction.tagName === "Alive") {
+      //           aliveProbability = prediction.probability;
+      //         } else if (prediction.tagName === "notalive") {
+      //           notAliveProbability = prediction.probability;
+      //         }
+      //       }
+
+      //       var result = '<div class="result-container">';
+      //       result += '<div class="prediction">';
+      //       result += '<p class="tag-name">Alive</p>';
+      //       result += '<div class="percentage-bar">';
+      //       result +=
+      //         '<div class="percentage" style="width: ' +
+      //         aliveProbability * 100 +
+      //         '%;"></div>';
+      //       result += "</div>";
+      //       result +=
+      //         '<p class="probability">' +
+      //         (aliveProbability * 100).toFixed(2) +
+      //         "%</p>";
+      //       result += "</div>";
+
+      //       result += '<div class="prediction">';
+      //       result += '<p class="tag-name">Not Alive</p>';
+      //       result += '<div class="percentage-bar">';
+      //       result +=
+      //         '<div class="percentage" style="width: ' +
+      //         notAliveProbability * 100 +
+      //         '%;"></div>';
+      //       result += "</div>";
+      //       result +=
+      //         '<p class="probability">' +
+      //         (notAliveProbability * 100).toFixed(2) +
+      //         "%</p>";
+      //       result += "</div>";
+      //       result += "</div>";
+
+      //       resultContainer.innerHTML = result;
+
+      //       if (aliveProbability > notAliveProbability) {
+      //         resultHeading.innerHTML =
+      //           '<h2 class="result-heading">This Video contains live person</h2>';
+      //       } else {
+      //         resultHeading.innerHTML =
+      //           '<h2 class="result-heading">This Video fails liveness check</h2>';
+      //       }
+      //     } else {
+      //       resultContainer.innerHTML = "<p>No predictions found.</p>";
+      //     }
+      //   },
 
       error: function () {
         var resultContainer = document.getElementById("resultContainer");
